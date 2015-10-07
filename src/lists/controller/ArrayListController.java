0d7bd46a5d470087d29c6772e5af3fa472b1af60
smartman97 <smartman97@gmail.com>
2015-10-07 09:48:56 -0600

    Prints list

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,9 +1,19 @@
 package lists.controller;
 
+import java.util.ArrayList;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("lol");
+		System.out.println(firstWords);
 	}
 }
